c1c2068e80084b85654db4fb8d27adde6517040e
AHerda <adrianherda@gmail.com>
2024-03-25 09:51:02 +0100

    Dodana tabela

--- lista2/zad4.pl
+++ lista2/zad4.pl
@@ -1,31 +1,20 @@
-% ==== utils ====
-% Długość Listy
-len([], 0).
-len([_ | L], N) :-
-	len(L, N1),
-	N = N1 + 1.
-
-% Zliczba ilość elementów między dwoma wystąpieniami elementu X
-liczba_elementow_miedzy(X, L, N) :-
-	append(_, [X | L1], L),
-	append(L2, [X | _], L1),
-	len(L2, N).
-
-% Sprawdza czy liczba elementów między dwoma wystąpieniami elementu X jest parzysta
-parzysta_liczba_miedzy(_, []).
-parzysta_liczba_miedzy(X, L) :-
-    liczba_elementow_miedzy(X, L, N),
-    N mod 2 =:= 0.
-
-% Dla każdej liczby parzystej od 1 do N sprawdza czy występuje parzysta liczba elementów między dwoma wystąpieniami
-dla_kazdego_parzysta_liczba_pomiedzy(0, _).
-dla_kazdego_parzysta_liczba_pomiedzy(N, X) :-
-	N > 0,
-	parzysta_liczba_miedzy(N, X),
-	N1 = N - 1,
-	dla_kazdego_parzysta_liczba_pomiedzy(N1, X).
-
-% ==== main ====
 lista(N, X) :-
-	len(X, 2 * N),
-	dla_kazdego_parzysta_liczba_pomiedzy(N, X).
+    M is N * 2,
+    length(X, M),
+    iter(N, X).
+
+iter(1, X) :-
+    check(1, X).
+iter(N, X) :-
+    M is N - 1,
+    check(N, X),
+    iter(M, X).
+
+check(N, L) :-
+    nth0(Ind1, L, N),
+    nth0(Ind2, L, N),
+    Ind1 < Ind2,
+    Min is N - 1,
+    Max is 2 * Min,
+    between(Min, Max, Ind1),
+    (Ind2 - Ind1) mod 2 =:= 1.
