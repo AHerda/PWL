36a8fb3333bb5d613bd0db95de5ac897480ef835
AHerda <adrianherda@gmail.com>
2024-03-25 09:59:25 +0100

    Mała poprawka w zad 4

--- lista2/zad4.pl
+++ lista2/zad4.pl
@@ -1,20 +1,20 @@
 lista(N, X) :-
     M is N * 2,
     length(X, M),
-    iter(N, X).
+    pom(N, X).
 
-iter(1, X) :-
-    check(1, X).
-iter(N, X) :-
+pom(1, X) :-
+    sprawdz(1, X).
+pom(N, X) :-
+    sprawdz(N, X),
     M is N - 1,
-    check(N, X),
-    iter(M, X).
+    pom(M, X).
 
-check(N, L) :-
-    nth0(Ind1, L, N),
-    nth0(Ind2, L, N),
-    Ind1 < Ind2,
+sprawdz(N, L) :-
+    nth0(I1, L, N),
+    nth0(I2, L, N),
+    I1 < I2,
+    (I2 - I1) mod 2 =:= 1,
     Min is N - 1,
     Max is 2 * Min,
-    between(Min, Max, Ind1),
-    (Ind2 - Ind1) mod 2 =:= 1.
+    between(Min, Max, I1).
